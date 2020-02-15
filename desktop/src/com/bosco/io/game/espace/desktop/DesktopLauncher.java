06c50e51ea35816428bd92e55c4a4f003c26458b
Axel <axel.bosco@usherbrooke.ca>
2020-02-15 18:25:46 -0500

    Created project

---
+++ desktop/src/com/bosco/io/game/espace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bosco.io.game.espace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bosco.io.game.espace.EscapeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EscapeGame(), config);
+	}
+}
